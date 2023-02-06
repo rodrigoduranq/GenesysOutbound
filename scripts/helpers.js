975b7950467c99f811446f9565ccf02944e13f50
rodrigoduranq <46093264+rodrigoduranq@users.noreply.github.com>
2023-02-05 20:26:03 -0600

    Mas cambios

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -16,35 +16,5 @@ var helpers = {
 				'</tr>';
 			$('#queuesTableBody').append(tableRow);
 		})
-	},
-	formatCellData: function(data, type) {
-		if (!data) return '';
-		switch(type) {
-			case 'date': {
-				try {
-					var date = new Date(data);
-
-					// http://stackoverflow.com/questions/25275696/javascript-format-date-time
-					var hours = date.getHours();
-					var minutes = date.getMinutes();
-					var ampm = hours >= 12 ? 'pm' : 'am';
-					hours = hours % 12;
-					hours = hours ? hours : 12; // the hour '0' should be '12'
-					minutes = minutes < 10 ? '0'+minutes : minutes;
-					var strTime = hours + ':' + minutes + ' ' + ampm;
-
-					// Include date if not today
-					if (date.getDate() != (new Date()).getDate())
-						return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
-					else
-						return strTime;
-				} catch(error) {
-					console.error(error);
-				}
-			}
-			default: {
-				return data.toString();
-			}
-		}
 	}
 };
