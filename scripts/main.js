7f12aa133d5d8ebdd8619becdc870c535151923e
rodrigoduranq <46093264+rodrigoduranq@users.noreply.github.com>
2023-02-06 19:31:47 -0600

    gffg

--- scripts/main.js
+++ scripts/main.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 			// Initialize API instances
 			usersApi = new purecloud.platform.UsersApi(pureCloudSession);
 			routingApi = new purecloud.platform.RoutingApi(pureCloudSession);
-			outboundApi = new purecloud.platform.OutboundApi(pureCloudSession);
+			outboundApi = new purecloud.platform.OutbondApi(pureCloudSession);
 
      prueba();
 			// Get the user's data (to verify token) and return the promise
