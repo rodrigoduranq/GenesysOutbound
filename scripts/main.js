001b6bace71009d5005b02e73ab0e6c9a711d96b
rodrigoduranq <46093264+rodrigoduranq@users.noreply.github.com>
2023-02-11 12:46:19 -0600

    ddd

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ const platformClient = require('platformClient');
 var client = platformClient.ApiClient.instance;
 client.loginImplicitGrant("60feb42b-6ef0-4761-ad7f-95ac491ee688", "window.location.href")
   .then(function() {
-    // Do authenticated things
+		console.log("FIRMADO");
   })
   .catch(function(err) {
     // Handle failure response
