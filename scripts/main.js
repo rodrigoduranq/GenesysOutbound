0572b11132329587311f6e7ec9d3166c4badeffa
rodrigoduranq <46093264+rodrigoduranq@users.noreply.github.com>
2023-02-11 11:53:43 -0600

    ddd

--- scripts/main.js
+++ scripts/main.js
@@ -60,9 +60,67 @@ pureCloudSession.login()
 	// Store the "me" object
 	_me = getMeResult;
 
+
+
+
 	// Get list of queues (function wraps API calls)
-//	return getQueues();
+	return getQueues();
 })
+.then(function(queuesList) {
+	// Save result
+	_queues = queuesList;
 
+	// Add to UI
+	helpers.displayQueuesList(_queues);
 
+	// Enable clipboard
+	new Clipboard('.clipbutton');
+})
+.catch(function(error){
+	console.error(error);
+});
+});
+
+// Get the list of queues. Returns a promise
+function getQueues(pageSize = 100, pageNumber = 1, sortBy, name, active) {
+return new Promise(function(fulfill, reject) {
+getQueuesImpl([], pageSize, pageNumber, sortBy, name, active, fulfill, reject);
+});
+}
+
+// Implementation of get queues to recursively get all queues and fulfill the promise when done
+function getQueuesImpl(queuesList, pageSize, pageNumber, sortBy, name, active, fulfill, reject) {
+// Invoke API
+routingApi.getQueues(pageSize, pageNumber, sortBy, name, active)
+	.then(function(getQueuesResponse) {
+		try {
+			// Append to list
+			$.each(getQueuesResponse.entities, function(index, queue) {
+				queuesList.push(queue);
+			});
+
+			if (getQueuesResponse.nextUri) {
+				// Recurse
+				console.log('Getting more queues from page ' + (getQueuesResponse.pageNumber + 1));
+				getQueuesImpl(queuesList,
+					getQueuesResponse.pageSize,
+					getQueuesResponse.pageNumber + 1,
+					sortBy,
+					name,
+					active,
+					fulfill,
+					reject);
+			} else {
+				// Fulfill promise
+				fulfill(queuesList);
+			}
+		} catch (error) {
+			console.log(error);
+			reject(error);
+		}
+	})
+	.catch(function(error){
+		console.log(error);
+		reject(error);
+	});
 }
