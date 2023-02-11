41882bd190f56127e2bc19959012654eeeab39f7
rodrigoduranq <46093264+rodrigoduranq@users.noreply.github.com>
2023-02-11 12:43:41 -0600

    gffg

--- scripts/main.js
+++ scripts/main.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
 
 
 
-
+/*
 
 	// Log debug info to the console
 	//pureCloudSession.debugLog = console.log;
@@ -289,3 +289,6 @@ function addOrUpdateConversation(queueId, conversation) {
 		}, 10 * 1000);
 	}
 }
+
+
+*/
