6249618bcff0d27b48239e5f488f9f07d0f67381
rodrigoduranq <46093264+rodrigoduranq@users.noreply.github.com>
2023-02-11 11:07:25 -0600

    dd

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,8 @@ var _queues = [];
 var _campas = [];
 var apiInstance;
 
+
+/*
   const platformClient = require('platformClient');
 
 	const client = platformClient.ApiClient.instance;
@@ -20,3 +22,20 @@ var apiInstance;
 	    // Handle failure response
 	    console.log(err);
 	  });
+
+*/
+
+pureCloudSession = purecloud.platform.PureCloudSession({
+	environment: 'mypurecloud.com',
+	strategy: 'implicit',
+	clientId: '60feb42b-6ef0-4761-ad7f-95ac491ee688',
+	redirectUrl: window.location.href,
+	storageKey: 'queue-notifications-example-auth-token',
+	timeout: 10000
+});
+
+// Log debug info to the console
+//pureCloudSession.debugLog = console.log;
+
+// Get auth token
+pureCloudSession.login()
