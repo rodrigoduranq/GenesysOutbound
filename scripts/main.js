f8626c628cbe94874e84429509b55e31a7111a7d
rodrigoduranq <46093264+rodrigoduranq@users.noreply.github.com>
2023-02-23 14:03:48 -0600

    gfgfgf

--- scripts/main.js
+++ scripts/main.js
@@ -202,29 +202,33 @@ document.getElementById("right").innerHTML = ch;
 
 ////////////////////
 
-const miCheckbox = document.getElementById("mi-checkbox");
-
-miCheckbox.addEventListener("change", function() {
-	if (miCheckbox.checked) {
-		miFuncionActivado();
-	} else {
-		miFuncionDesactivado();
-	}
-});
-
-function miFuncionActivado() {
-	console.log("El botón está activado");
-	// Aquí puedes agregar el código que se ejecutará cuando el botón esté activado
-}
-
-function miFuncionDesactivado() {
-	console.log("El botón está desactivado");
-	// Aquí puedes agregar el código que se ejecutará cuando el botón esté desactivado
-}
 
 
 
   })
+
+	const miCheckbox = document.getElementById("mi-checkbox");
+
+	miCheckbox.addEventListener("change", function() {
+		if (miCheckbox.checked) {
+			miFuncionActivado();
+		} else {
+			miFuncionDesactivado();
+		}
+	});
+
+	function miFuncionActivado() {
+		console.log("El botón está activado");
+		// Aquí puedes agregar el código que se ejecutará cuando el botón esté activado
+	}
+
+	function miFuncionDesactivado() {
+		console.log("El botón está desactivado");
+		// Aquí puedes agregar el código que se ejecutará cuando el botón esté desactivado
+	}
+
+
+
 }
 
 
