278b6d038d4280e6896cb877c0537d0fd1d2e90c
rodrigoduranq <46093264+rodrigoduranq@users.noreply.github.com>
2023-02-22 20:49:52 -0600

    fddf

--- scripts/main.js
+++ scripts/main.js
@@ -86,7 +86,7 @@ outboundApi.getOutboundContactlistsDivisionview(Campaign.contactList.id, opts)
     console.log ('Aqui va Data');
 		console.log (data);
 
-	ch = ch + "<table class="zebra" style="max-width:300px;""
+	ch = ch + "<table style="max-width:300px;">"
 	ch = ch + "<thead><tr>"
 
 		 claves.forEach (clave =>
